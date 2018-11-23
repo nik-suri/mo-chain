ee948b6549ca289c3d1e886dd2e67462aeefe3b2
Nikhil Suri <nikhilsuri@comcast.net>
2018-11-22 23:54:25 -0800

    clarity in functions, removed dev-test, updated package.json

--- chain-util.js
+++ chain-util.js
@@ -4,18 +4,23 @@ const uuidV1 = require('uuid/v1');
 const ec = new EC('secp256k1');
 
 class ChainUtil {
+
+  // generate public and private key pair
   static genKeyPair() {
     return ec.genKeyPair();
   }
 
+  // generate unique id
   static id() {
     return uuidV1();
   }
 
+  // generic hash function on some data
   static hash(data) {
     return SHA256(JSON.stringify(data)).toString();
   }
 
+  // verify hashed data using given publicKey and signature
   static verifySignature(publicKey, signature, dataHash) {
     return ec.keyFromPublic(publicKey, 'hex').verify(dataHash, signature);
   }
--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 // starting difficulty for proof of work
 const DIFFICULTY = 4;
+
 // seconds between blocks
 const MINE_RATE = 3000;
+
+// initial balance for user wallets
+// would normally be 0
 const INITIAL_BALANCE = 500;
 
 module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE };
