baf6e5b6a5a7ea8ddb796c6f12918f7dd66492b5
Nikhil Suri <nikhilsuri@comcast.net>
2018-11-22 23:02:57 -0800

    files added

---
+++ config.js
@@ -0,0 +1,7 @@
+// starting difficulty for proof of work
+const DIFFICULTY = 4;
+// seconds between blocks
+const MINE_RATE = 3000;
+const INITIAL_BALANCE = 500;
+
+module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE };
